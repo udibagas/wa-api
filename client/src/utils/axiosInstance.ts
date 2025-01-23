d461e0c8af427f029b8133a77702cdfb32c0ac49
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-01-23 17:03:28 +0700

    make sure token exists

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -4,8 +4,20 @@ const axiosInstance = axios.create({
   baseURL: "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
