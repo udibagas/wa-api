a97dcaeca13bedbd931eac79e2ae3f88c55eaca0
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-02-02 08:31:34 +0700

    jos

--- client/src/hooks/useCrud.ts
+++ client/src/hooks/useCrud.ts
@@ -1,29 +1,57 @@
 import { useState, useEffect } from "react";
-import { message, Modal } from "antd";
+import { FormInstance, message, Modal } from "antd";
 import axiosInstance from "../utils/axiosInstance";
+import { AxiosError } from "axios";
+import { Form } from "antd";
+import { AxiosErrorResponseType, RecursivePartial } from "../types";
 
 type CrudHook<T> = {
   data: T[];
+  form: FormInstance<T>;
   errors: Record<string, string[]>;
+  isEditing: boolean;
+  isModalVisible: boolean;
   setErrors: (errors: Record<string, string[]>) => void;
   fetchData: () => void;
   addItem: (item: T) => Promise<void>;
   updateItem: (id: number, item: T) => Promise<void>;
   deleteItem: (id: number) => Promise<void>;
-  showDeleteConfirm: (userId: number) => void;
+  showDeleteConfirm: (id: number) => void;
+  setIsEditing: (isEditing: boolean) => void;
+  setIsModalVisible: (isVisible: boolean) => void;
+  handleAdd: () => void;
+  handleEdit: (data: T) => void;
+  handleModalOk: (values: T) => void;
+  handleModalClose: () => void;
 };
 
 const useCrud = <T extends { id: number }>(endpoint: string): CrudHook<T> => {
   const [data, setData] = useState<T[]>([]);
   const [errors, setErrors] = useState<Record<string, string[]>>({});
+  const [form] = Form.useForm<T>();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
   const fetchData = async () => {
     try {
       const response = await axiosInstance.get(endpoint);
       setData(response.data);
-    } catch (error: any) {
-      message.error(error.message);
+    } catch (error) {
+      message.error((error as AxiosError).message);
     }
+  };
+
+  const handleError = (error: unknown) => {
+    const axiosError = error as AxiosError;
+    const axiosErrorResponse = axiosError.response
+      ?.data as AxiosErrorResponseType;
+
+    if (axiosError.response?.status === 400) {
+      setErrors(axiosErrorResponse.errors ?? {});
+    }
+
+    message.error(axiosErrorResponse.message ?? axiosError.message);
+    throw error; // rethrow the error so that the caller can handle it
   };
 
   const addItem = async (item: T) => {
@@ -31,13 +59,8 @@ const useCrud = <T extends { id: number }>(endpoint: string): CrudHook<T> => {
       await axiosInstance.post(endpoint, item);
       message.success("Item added successfully");
       fetchData();
-    } catch (error: any) {
-      if (error.response?.status === 400) {
-        setErrors(error.response?.data?.errors ?? {});
-      }
-
-      message.error(error.response?.data?.message ?? error.message);
-      throw error;
+    } catch (error: unknown) {
+      handleError(error);
     }
   };
 
@@ -46,13 +69,8 @@ const useCrud = <T extends { id: number }>(endpoint: string): CrudHook<T> => {
       await axiosInstance.put(`${endpoint}/${id}`, item);
       message.success("Item updated successfully");
       fetchData();
-    } catch (error: any) {
-      if (error.response?.status === 400) {
-        setErrors(error.response?.data?.errors ?? {});
-      }
-
-      message.error(error.response?.data?.message ?? error.message);
-      throw error;
+    } catch (error: unknown) {
+      handleError(error);
     }
   };
 
@@ -61,8 +79,8 @@ const useCrud = <T extends { id: number }>(endpoint: string): CrudHook<T> => {
       await axiosInstance.delete(`${endpoint}/${id}`);
       message.success("Item deleted successfully");
       fetchData();
-    } catch (error: any) {
-      message.error(error.response?.data?.message ?? error.message);
+    } catch (error) {
+      handleError(error);
     }
   };
 
@@ -77,19 +95,62 @@ const useCrud = <T extends { id: number }>(endpoint: string): CrudHook<T> => {
     });
   };
 
+  const handleAdd = () => {
+    setIsEditing(false);
+    form.resetFields();
+    setIsModalVisible(true);
+  };
+
+  const handleEdit = (data: T) => {
+    setIsEditing(true);
+    form.setFieldsValue(data as RecursivePartial<T>);
+    setIsModalVisible(true);
+  };
+
+  const handleModalOk = async (values: T) => {
+    try {
+      if (values.id) {
+        await updateItem(values.id, values);
+      } else {
+        await addItem(values);
+      }
+      handleModalClose();
+    } catch (error) {
+      console.log((error as Error).message);
+    }
+  };
+
+  const handleModalClose = () => {
+    setErrors({});
+    setIsModalVisible(false);
+  };
+
   useEffect(() => {
     fetchData();
-  }, [endpoint]);
+
+    return () => {
+      setData([]);
+    };
+  }, []);
 
   return {
     data,
     errors,
+    form,
+    isModalVisible,
+    isEditing,
     setErrors,
     fetchData,
     addItem,
     updateItem,
     deleteItem,
     showDeleteConfirm,
+    setIsEditing,
+    setIsModalVisible,
+    handleAdd,
+    handleEdit,
+    handleModalOk,
+    handleModalClose,
   };
 };
 
