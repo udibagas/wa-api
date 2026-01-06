ce35b33b43fad9ffc16df9dad2efc5a167c74d42
udibagas <udibagas@gmail.com>
2026-01-06 20:42:37 +0700

    refactor: comment out unused imports and functions in Template component

--- client/src/pages/Template.tsx
+++ client/src/pages/Template.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { message, Modal, Table } from "antd";
-import { CheckOutlined, ReloadOutlined } from "@ant-design/icons";
+// import { message, Modal, Table } from "antd";
+import { Table } from "antd";
+// import { CheckOutlined, ReloadOutlined } from "@ant-design/icons";
+import { ReloadOutlined } from "@ant-design/icons";
 import PageHeader from "../components/PageHeader";
 import AddButton from "../components/buttons/AddButton";
 import ActionButton from "../components/buttons/ActionButton";
 import { TemplateType } from "../types";
 import TemplateForm from "../components/TemplateForm";
 import useCrud from "../hooks/useCrud";
-import client from "../api/client";
-import StatusTag from "../components/StatusTag";
+// import client from "../api/client";
 
 const Template: React.FC = () => {
   const {
@@ -27,34 +28,31 @@ const Template: React.FC = () => {
 
   const { isPending, data } = useFetch();
 
-  const submit = (record: TemplateType) => {
-    Modal.confirm({
-      title: "Confirm",
-      content: "Are you sure you want to submit this template? This action cannot be undone.",
-      okText: "Yes",
-      okType: "primary",
-      cancelText: "No",
-      onOk: () => submitTemplate(record),
-    })
-  }
+  // const submit = (record: TemplateType) => {
+  //   Modal.confirm({
+  //     title: "Confirm",
+  //     content: "Are you sure you want to submit this template? This action cannot be undone.",
+  //     okText: "Yes",
+  //     okType: "primary",
+  //     cancelText: "No",
+  //     onOk: () => submitTemplate(record),
+  //   })
+  // }
 
-  const submitTemplate = async (record: TemplateType) => {
-    try {
-      await client.patch(`/message-templates/${record.id}/submit`);
-      message.success("Template submitted successfully");
-      refreshData();
-    } catch (error) {
-      message.error((error as Error).message);
-    }
-  }
+  // const submitTemplate = async (record: TemplateType) => {
+  //   try {
+  //     await client.patch(`/message-templates/${record.id}/submit`);
+  //     message.success("Template submitted successfully");
+  //     refreshData();
+  //   } catch (error) {
+  //     message.error((error as Error).message);
+  //   }
+  // }
 
   const columns = [
     { title: "ID", dataIndex: 'id', key: "id", width: 60 },
     { title: "App", dataIndex: ["App", "name"], key: "app" },
     { title: "Name", dataIndex: "name", key: "name" },
-    {
-      title: "Status", render: (_: string, record: TemplateType) => <StatusTag status={record.status} />
-    },
     {
       title: <ReloadOutlined onClick={refreshData} />,
       key: "action",
@@ -64,9 +62,9 @@ const Template: React.FC = () => {
         <ActionButton
           onEdit={() => handleEdit(record)}
           onDelete={() => handleDelete(record.id)}
-          additionalItems={[
-            { key: "submit", label: 'Submit', icon: <CheckOutlined />, onClick: () => submit(record) }
-          ]}
+        // additionalItems={[
+        //   { key: "submit", label: 'Submit', icon: <CheckOutlined />, onClick: () => submit(record) }
+        // ]}
         />
       ),
     },
